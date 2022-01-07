e5894ce5e38e720bc80cfca82a3523ff884f0d01
Joshua Urrea <jurrea619@gmail.com>
2022-01-06 21:04:09 -0800

    Add JS hover effect on grid

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 const container = document.querySelector('.container');
 
-// loop and add 4x4 squares to begin grid.
+// build x amt of squares, attach mouseover listeners and append to grid container
 for(let i = 0 ; i < 256 ; i++){
+    //create div with 'box' class
     let newDiv = document.createElement('div');
     newDiv.classList.add('box');
+
+    //attach mouseover listener event to turn background color black
+    newDiv.addEventListener('mouseenter', () =>{
+        newDiv.style.backgroundColor = 'black';
+    });
+
+    // add to container
     container.appendChild(newDiv);
 }
