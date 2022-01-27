44912d46be59f7e148c3d8073888af7500926bea
Joshua Urrea <jurrea619@gmail.com>
2022-01-26 17:00:28 -0800

    Implement size button to accept user re-sizing up to 35x35

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.grid');
 
-// build x amt of squares, attach mouseover listeners and append to grid container
+// build initial grid
 for(let i = 0 ; i < 256 ; i++){
     //create div with 'box' class
     let newDiv = document.createElement('div');
@@ -15,12 +15,47 @@ for(let i = 0 ; i < 256 ; i++){
     container.appendChild(newDiv);
 }
 
-// add event listener to button
-// when clicked all elements with class 'box' set to gray
-const resetBtn = document.querySelector("#reset");
-resetBtn.addEventListener("click", ()=> {
+// reset button changes grid to brand new
+const resetBtn = document.querySelector("#resetBtn");
+resetBtn.addEventListener("click", clearScreen);
+
+function clearScreen(){
     const boxes = document.querySelectorAll(".box");
-    boxes.forEach( (button) => {
+    boxes.forEach((button) => {
         button.style.backgroundColor = "lightslategray";
     });
+}
+
+// grab size button and grid element to be adjusted
+const sizeBtn = document.querySelector("#sizeBtn");
+const grid = 
+sizeBtn.addEventListener("click", ()=> {
+    let newSize = prompt("Enter new grid size (up to 35)");    
+    //edit grid-template-columns & grid-template rows
+    container.style['grid-template-columns'] = `repeat(${newSize}, 1fr`;
+    // container.style['grid-template-rows'] = `repeat(${newSize}), 1fr`;
+
+    //remove all existing boxes from grid
+    removeGridNodes();
+
+    for (let i = 0; i < newSize ** 2; i++) {
+        //create div with 'box' class
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('box');
+
+        //attach mouseover listener event to turn background color black
+        newDiv.addEventListener('mouseenter', () => {
+            newDiv.style.backgroundColor = 'black';
+        });
+
+        // add to container
+        container.appendChild(newDiv);
+    }
 });
+
+// remove all child nodes from container
+function removeGridNodes() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
