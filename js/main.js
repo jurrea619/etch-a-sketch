1f3de6b941b1b9e00af85e4ea5a9da2807a7ceb7
Joshua Urrea <jurrea619@gmail.com>
2022-03-27 20:15:37 -0700

    Refactor JS script. Add documentation. Minor CSS edits

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
+// declare var and attach grid container
 const container = document.querySelector('.grid');
 
-// build initial grid
+// build initial 16*16 grid on page load
 for(let i = 0 ; i < 256 ; i++){
-    //create div with 'box' class
     let newDiv = document.createElement('div');
     newDiv.classList.add('box');
 
@@ -11,14 +11,17 @@ for(let i = 0 ; i < 256 ; i++){
         newDiv.style.backgroundColor = 'black';
     });
 
-    // add to container
+    // add div to container
     container.appendChild(newDiv);
 }
 
-// reset button changes grid to brand new
+// declare var for reset button, attach click event handler function
 const resetBtn = document.querySelector("#resetBtn");
 resetBtn.addEventListener("click", clearScreen);
 
+/**
+ * Resets current grid back to original grey color
+ */
 function clearScreen(){
     const boxes = document.querySelectorAll(".box");
     boxes.forEach((button) => {
@@ -28,17 +31,24 @@ function clearScreen(){
 
 // grab size button and grid element to be adjusted
 const sizeBtn = document.querySelector("#sizeBtn");
-const grid = 
 sizeBtn.addEventListener("click", ()=> {
     let newSize = prompt("Enter new grid size (up to 100)");    
+    resizeGrid(newSize);
+});
+
+/**
+ * Create new grid with size*size
+ * 
+ * @param {number} size size of new grid rows/columns
+ */
+function resizeGrid(size){
     //edit grid-template-columns & grid-template rows
-    container.style['grid-template-columns'] = `repeat(${newSize}, 1fr`;
-    // container.style['grid-template-rows'] = `repeat(${newSize}), 1fr`;
+    container.style['grid-template-columns'] = `repeat(${size}, 1fr`;
 
     //remove all existing boxes from grid
     removeGridNodes();
 
-    for (let i = 0; i < newSize ** 2; i++) {
+    for (let i = 0; i < size ** 2; i++) {
         //create div with 'box' class
         let newDiv = document.createElement('div');
         newDiv.classList.add('box');
@@ -51,9 +61,11 @@ sizeBtn.addEventListener("click", ()=> {
         // add to container
         container.appendChild(newDiv);
     }
-});
+}
 
-// remove all child nodes from container
+/**
+ * Remove all grid nodes from grid container
+ */
 function removeGridNodes() {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
